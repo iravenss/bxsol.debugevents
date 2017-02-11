a4b0e4e525aefb22a2b69ff7094989b32081bbaa
don_tre <lexa@skater.ru>
2017-02-11 16:35:41 +0300

    sort field

--- admin/bxsol_debug_events.php
+++ admin/bxsol_debug_events.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Mineev Aleksey (2016 ©)
  * alekseym@bxsolutions.ru
@@ -19,7 +19,7 @@ $APPLICATION->SetTitle(Loc::getMessage('BXSOL_DEBUG_EVENTS_TITLE'));
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_after.php");
 
 $manager = New CDebugEvents;
-$events = $manager->getEvents();
+$events  = $manager->getEvents();
 
 if (!is_array($events))
 {
@@ -29,51 +29,38 @@ if (!is_array($events))
 $allEvents = array();
 
 ?>
+    <div class="bxsol__support">
+        <a target="_blank" class="bxsol__support__rate_us"
+           href="http://marketplace.1c-bitrix.ru/bxsol.debugevents"><?= Loc::getMessage('BXSOL__SUPPORT_RATE_US') ?></a>
+        <a target="_blank" class="bxsol__support__write_us"
+           href="http://bitrixsolutions.ru/"><?= Loc::getMessage('BXSOL__SUPPORT_WRITE_US') ?></a>
+    </div>
 
-	<div class="bxsol__support">
-		<a target="_blank" class="bxsol__support__rate_us"
-		   href="http://marketplace.1c-bitrix.ru/bxsol.debugevents"><?= Loc::getMessage('BXSOL__SUPPORT_RATE_US') ?></a>
-		<a target="_blank" class="bxsol__support__write_us"
-		   href="http://bitrixsolutions.ru/"><?= Loc::getMessage('BXSOL__SUPPORT_WRITE_US') ?></a>
-	</div>
-
-	<input type="text" id="bxsol_events__search" class="bxsol_events__search"
-		   placeholder="<?= Loc::getMessage('BXSOL_DEBUG_EVENTS_FIND') ?>">
+    <input type="text" id="bxsol_events__search" class="bxsol_events__search"
+           placeholder="<?= Loc::getMessage('BXSOL_DEBUG_EVENTS_FIND') ?>">
 <?php
 
 foreach ($events as $module_id => $events)
 {
-
 	$module_info = $manager->getModuleInfo($module_id);
-
-	if (isset($module_info['MODULE_NAME']))
-	{
-		$label = $module_info['MODULE_NAME'] . ' (' . $module_id . ')';
-	}
-	else
-	{
-		$label = $module_id;
-	}
+	$label       = isset($module_info['MODULE_NAME']) ? $module_info['MODULE_NAME'] . ' (' . $module_id . ')' : $module_id;
 
 	?>
-	<div class="bxsol_events">
+    <div class="bxsol_events">
 
-		<h1 class="bxsol_events__module_header"><?= $label ?></h1>
+        <h1 class="bxsol_events__module_header"><?= $label ?></h1>
 		<?php
 		ksort($events);
 
 		foreach ($events as $event_name => $handlers)
 		{
-
 			$event_name = strtolower($event_name);
-
 			$allEvents[] = $event_name;
 
 			echo '<div class="ev__' . $event_name . ' bxsol_events__event"><div class="bxsol_events__event_name">' . CDebugEvents::formatEvent(strtoupper($event_name)) . ' </div>' . '<div class="bxsol_events__handlers">';
 
 			foreach ($handlers as $h)
 			{
-
 				$class = $method = $handler = $file_path = $line = $module_id = '';
 
 				if (isset($h['TO_MODULE_ID']))
@@ -92,9 +79,8 @@ foreach ($events as $module_id => $events)
 					}
 
 				}
-				elseif (isset($h['CALLBACK']) && !empty($h['CALLBACK']))
+                elseif (isset($h['CALLBACK']) && !empty($h['CALLBACK']))
 				{
-
 					if (is_array($h['CALLBACK']))
 					{
 						if (isset($h['CALLBACK'][0]))
@@ -105,7 +91,6 @@ foreach ($events as $module_id => $events)
 						if (isset($h['CALLBACK'][1]))
 						{
 							$method = $h['CALLBACK'][1];
-
 						}
 					}
 					else
@@ -113,7 +98,7 @@ foreach ($events as $module_id => $events)
 						$method = $h['CALLBACK'];
 					}
 				}
-				elseif (isset($h['TO_PATH']) && !empty($h['TO_PATH']))
+                elseif (isset($h['TO_PATH']) && !empty($h['TO_PATH']))
 				{
 					$file_path = $h['TO_PATH'];
 				}
@@ -131,24 +116,28 @@ foreach ($events as $module_id => $events)
 				{
 					$reflFunc = new ReflectionMethod($class, $method);
 				}
-				elseif ($method != '' && function_exists($method))
+                elseif ($method != '' && function_exists($method))
 				{
 					$reflFunc = new ReflectionFunction($method);
 				}
 
 				if (!is_null($reflFunc))
 				{
-
 					$file_path = $reflFunc->getFileName();
 
 					$file_path = str_replace('\\', '/', $file_path);
 					$file_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $file_path);
-					$line = $reflFunc->getStartLine();
+					$line      = $reflFunc->getStartLine();
+				}
+
+				if (!empty($h['SORT']))
+				{
+					$handler .= '<span class="bxsol_events__handlers__sort" title="sort order">' . $h['SORT'] . '</span>';
 				}
 
 				if ($class != '')
 				{
-					$handler = '<span class="bxsol_events__class">' . $class . ($method != '' ? '::' : '') . '</span>';
+					$handler .= '<span class="bxsol_events__class">' . $class . ($method != '' ? '::' : '') . '</span>';
 				}
 
 				if ($method != '')
@@ -165,7 +154,7 @@ foreach ($events as $module_id => $events)
 				if ($file_path != '')
 				{
 					$file_name = $file_path;
-					$handler .= ' <a target="_blank" class="bxsol_events__class_source" href="/bitrix/admin/fileman_file_edit.php?full_src=Y&path=' . urlencode($file_path) . '" title="'.Loc::getMessage('BXSOL_DEBUG_EVENTS_GOTO_FILE').'">' . $file_name . ':' . $line . '</a>';
+					$handler   .= ' <a target="_blank" class="bxsol_events__class_source" href="/bitrix/admin/fileman_file_edit.php?full_src=Y&path=' . urlencode($file_path) . '" title="' . Loc::getMessage('BXSOL_DEBUG_EVENTS_GOTO_FILE') . '">' . $file_name . ':' . $line . '</a>';
 				}
 
 				echo '<p class="bxsol_events__handlers__hadler">' . $handler . '</p>';
@@ -174,15 +163,15 @@ foreach ($events as $module_id => $events)
 		}
 		?>
 
-	</div>
+    </div>
 
 	<?php
 }
 
 ?>
-	<script>
-		var bxsol_debug_events = <?=json_encode($allEvents)?>;
-	</script>
+    <script>
+        var bxsol_debug_events = <?=json_encode($allEvents)?>;
+    </script>
 
 <?php
 
